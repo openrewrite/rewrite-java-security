f07bc18b86dc60460d48f5fb489a38c2619fd976
Jonathan Schneider <jkschneider@gmail.com>
2021-09-12 11:14:41 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+![Logo](https://github.com/openrewrite/rewrite/raw/main/doc/logo-oss.png)
+### Patch Java security vulnerabilities. Automatically.
+
+[![ci](https://github.com/openrewrite/rewrite-java-security/actions/workflows/ci.yml/badge.svg)](https://github.com/openrewrite/rewrite-java-security/actions/workflows/ci.yml)
+[![Apache 2.0](https://img.shields.io/github/license/openrewrite/rewrite-java-security.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+[![Maven Central](https://img.shields.io/maven-central/v/org.openrewrite.recipe/rewrite-java-security.svg)](https://mvnrepository.com/artifact/org.openrewrite.recipe/rewrite-java-security)
+
+### What is this?
+
+This project implements a [Rewrite module](https://github.com/openrewrite/rewrite) that patches common Java security vulnerabilities.
---
+++ build.gradle.kts
@@ -0,0 +1,162 @@
+import com.github.jk1.license.LicenseReportExtension
+import nebula.plugin.contacts.Contact
+import nebula.plugin.contacts.ContactsExtension
+import nl.javadude.gradle.plugins.license.LicenseExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
+    id("nebula.release") version "15.3.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+
+    id("com.github.hierynomus.license") version "0.16.1"
+    id("com.github.jk1.dependency-license-report") version "1.16"
+
+    id("nebula.maven-publish") version "17.3.2"
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.3.0"
+
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+    id("nebula.maven-apache-license") version "17.3.2"
+
+    id("org.openrewrite.rewrite") version "latest.integration"
+}
+
+apply(plugin = "nebula.publish-verification")
+
+rewrite {
+    rewriteVersion = "latest.integration"
+    activeRecipe("org.openrewrite.java.format.AutoFormat", "org.openrewrite.java.cleanup.Cleanup")
+}
+
+configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
+    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
+}
+
+group = "org.openrewrite.recipe"
+description = "Enforce logging best practices and migrate between logging frameworks. Automatically."
+
+repositories {
+    if (!project.hasProperty("releasing")) {
+        mavenLocal()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+    }
+    mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+signing {
+    setRequired({
+        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
+    })
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["nebula"])
+}
+
+configurations.all {
+    resolutionStrategy {
+        cacheChangingModulesFor(0, TimeUnit.SECONDS)
+        cacheDynamicVersionsFor(0, TimeUnit.SECONDS)
+    }
+}
+
+val rewriteVersion = if (project.hasProperty("releasing")) {
+    "latest.release"
+} else {
+    "latest.integration"
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:latest.release")
+    annotationProcessor("org.projectlombok:lombok:latest.release")
+
+    implementation("org.openrewrite:rewrite-java:${rewriteVersion}")
+    implementation("org.openrewrite:rewrite-maven:${rewriteVersion}")
+    runtimeOnly("org.openrewrite:rewrite-java-11:${rewriteVersion}")
+
+    // eliminates "unknown enum constant DeprecationLevel.WARNING" warnings from the build log
+    // see https://github.com/gradle/kotlin-dsl-samples/issues/1301 for why (okhttp is leaking parts of kotlin stdlib)
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
+
+    testImplementation("org.openrewrite:rewrite-test:${rewriteVersion}")
+
+    testImplementation("org.assertj:assertj-core:latest.release")
+
+    testRuntimeOnly("org.openrewrite:rewrite-java-11:${rewriteVersion}")
+    testRuntimeOnly("org.openrewrite:rewrite-java-8:${rewriteVersion}")
+}
+
+tasks.withType(KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+
+    options.isFork = true
+    options.forkOptions.executable = "javac"
+    options.compilerArgs.addAll(listOf("--release", "8"))
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
+
+
+configure<ContactsExtension> {
+    val j = Contact("team@moderne.io")
+    j.moniker("Team Moderne")
+
+    people["team@moderne.io"] = j
+}
+
+configure<LicenseExtension> {
+    ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
+    skipExistingHeaders = true
+    header = project.rootProject.file("gradle/licenseHeader.txt")
+    mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
+    // exclude JavaTemplate shims from license check
+    exclude("src/main/resources/META-INF/rewrite/*.java")
+    strictCheck = true
+}
+
+configure<LicenseReportExtension> {
+    renderers = arrayOf(com.github.jk1.license.render.CsvReportRenderer())
+}
+
+configure<PublishingExtension> {
+    publications {
+        named("nebula", MavenPublication::class.java) {
+            suppressPomMetadataWarningsFor("runtimeElements")
+        }
+    }
+}
