5901ba0b585d5e6b93cb6f83c538bfe338a0fb7f
Jonathan Schneider <jkschneider@gmail.com>
2021-09-13 12:05:03 -0700

    License headers

--- src/main/java/org/openrewrite/java/security/SecureRandom.java
+++ src/main/java/org/openrewrite/java/security/SecureRandom.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.security;
 
 import org.openrewrite.ExecutionContext;
@@ -5,7 +20,9 @@ import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.search.InJavaSourceSet;
+import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.TypeUtils;
 
@@ -29,7 +46,14 @@ public class SecureRandom extends Recipe {
 
     @Override
     protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
-        return new InJavaSourceSet<>("main");
+        return new JavaVisitor<ExecutionContext>() {
+            @Override
+            public J visitCompilationUnit(J.CompilationUnit cu, ExecutionContext ctx) {
+                doAfterVisit(new UsesType<>("java.util.Random"));
+                doAfterVisit(new InJavaSourceSet<>("main"));
+                return cu;
+            }
+        };
     }
 
     @Override
