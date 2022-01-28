2a417c5895b5ba465efb1b2b7f5f93b6bd0501aa
Patrick <patway99@gmail.com>
2022-01-28 11:15:27 -0800

    Revert "Update SecureTempFileCreation to convert File.CreateTempFile to Files.createTempDirectory.  Fixes #1353"
    This reverts commit 5fef3338fc8af783ef6dae794975587507068db3.

--- src/test/kotlin/org/openrewrite/java/security/SecureTempFileCreationTest.kt
+++ src/test/kotlin/org/openrewrite/java/security/SecureTempFileCreationTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 the original author or authors.
+ * Copyright 2021 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ class SecureTempFileCreationTest : JavaRecipeTest {
 
             class Test {
                 static void method() throws IOException {
-                    File tempDir = Files.createTempDirectory("hello" + "world").toFile();
+                    File tempDir = Files.createTempFile("hello", "world").toFile();
                 }
             }
         """
@@ -69,7 +69,7 @@ class SecureTempFileCreationTest : JavaRecipeTest {
 
             class Test {
                 static void method() throws IOException {
-                    File tempDir = Files.createTempDirectory(new File(".").toPath(), "hello" + "world").toFile();
+                    File tempDir = Files.createTempFile(new File(".").toPath(), "hello", "world").toFile();
                 }
             }
         """
