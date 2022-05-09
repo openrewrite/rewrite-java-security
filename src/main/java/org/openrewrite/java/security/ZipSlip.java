6c8f64d06804fcdb8b2285d33fe434f86ab409e2
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2022-05-09 16:55:48 -0400

    Add ZipSlip Tests

---
+++ src/main/java/org/openrewrite/java/security/ZipSlip.java
@@ -0,0 +1,10 @@
+package org.openrewrite.java.security;
+
+import org.openrewrite.Recipe;
+
+public class ZipSlip extends Recipe {
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+}
