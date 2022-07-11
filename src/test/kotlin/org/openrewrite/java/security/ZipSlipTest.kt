9901f1d36a31754d3263e4cc7cdf0be78198da1f
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2022-07-10 22:23:20 -0400

    Add Zip Slip test example intranet_app_manager

--- src/test/kotlin/org/openrewrite/java/security/ZipSlipTest.kt
+++ src/test/kotlin/org/openrewrite/java/security/ZipSlipTest.kt
@@ -503,4 +503,113 @@ class ZipSlipTest: RewriteTest {
             """
         )
     )
+
+    @Test
+    fun `example intranet_app_manager`() = rewriteRun(
+        java(
+            """
+            package org.yzr.utils.file;
+
+            import java.io.File;
+            import java.io.FileOutputStream;
+            import java.io.InputStream;
+            import java.util.Enumeration;
+            import java.util.zip.ZipEntry;
+            import java.util.zip.ZipFile;
+
+            public class ZipUtil {
+
+                public static String unzip(String path) {
+                    try {
+                        long start = System.currentTimeMillis();
+                        String destDirPath = System.getProperty("java.io.tmpdir") + File.separator + start;
+                        ZipFile zipFile = new ZipFile(path);
+                        Enumeration<?> entries = zipFile.entries();
+                        while (entries.hasMoreElements()) {
+                            ZipEntry entry = (ZipEntry) entries.nextElement();
+                            if (entry.isDirectory()) {
+                                String dirPath = destDirPath + File.separator + entry.getName();
+                                File dir = new File(dirPath);
+                                dir.mkdirs();
+                            } else {
+                                File targetFile = new File(destDirPath + File.separator + entry.getName());
+                                if (!targetFile.getParentFile().exists()) {
+                                    targetFile.getParentFile().mkdirs();
+                                }
+                                targetFile.createNewFile();
+                                InputStream is = zipFile.getInputStream(entry);
+                                FileOutputStream fos = new FileOutputStream(targetFile);
+                                int len;
+                                byte[] buf = new byte[1024];
+                                while ((len = is.read(buf)) != -1) {
+                                    fos.write(buf, 0, len);
+                                }
+                                fos.close();
+                                is.close();
+                            }
+                        }
+                        zipFile.close();
+                        return destDirPath;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+            }
+            """,
+            """
+            package org.yzr.utils.file;
+
+            import java.io.File;
+            import java.io.FileOutputStream;
+            import java.io.InputStream;
+            import java.util.Enumeration;
+            import java.util.zip.ZipEntry;
+            import java.util.zip.ZipFile;
+
+            public class ZipUtil {
+
+                public static String unzip(String path) {
+                    try {
+                        long start = System.currentTimeMillis();
+                        String destDirPath = System.getProperty("java.io.tmpdir") + File.separator + start;
+                        ZipFile zipFile = new ZipFile(path);
+                        Enumeration<?> entries = zipFile.entries();
+                        while (entries.hasMoreElements()) {
+                            ZipEntry entry = (ZipEntry) entries.nextElement();
+                            if (entry.isDirectory()) {
+                                String dirPath = destDirPath + File.separator + entry.getName();
+                                File dir = new File(dirPath);
+                                dir.mkdirs();
+                            } else {
+                                File targetFile = new File(destDirPath, entry.getName());
+                                if (!targetFile.toPath().normalize().startsWith(destDirPath.toPath())) {
+                                    throw new RuntimeException("Bad zip entry");
+                                }
+                                if (!targetFile.getParentFile().exists()) {
+                                    targetFile.getParentFile().mkdirs();
+                                }
+                                targetFile.createNewFile();
+                                InputStream is = zipFile.getInputStream(entry);
+                                FileOutputStream fos = new FileOutputStream(targetFile);
+                                int len;
+                                byte[] buf = new byte[1024];
+                                while ((len = is.read(buf)) != -1) {
+                                    fos.write(buf, 0, len);
+                                }
+                                fos.close();
+                                is.close();
+                            }
+                        }
+                        zipFile.close();
+                        return destDirPath;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+            }
+            """
+        )
+    )
 }
