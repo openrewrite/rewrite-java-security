046105f30013ec2ca63cbc961cf3d33487707ac5
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2022-07-16 22:39:35 +0200

    Further improve Zip Slip fix with Partial Path Traversal

--- src/test/kotlin/org/openrewrite/java/security/ZipSlipTest.kt
+++ src/test/kotlin/org/openrewrite/java/security/ZipSlipTest.kt
@@ -734,4 +734,132 @@ class ZipSlipTest: RewriteTest {
             """
         )
     )
+
+    @Test
+    fun `example fortify-coudscan-plugin`() = rewriteRun(
+        java(
+            """
+            import java.io.File;
+            import java.io.FileInputStream;
+            import java.io.FileNotFoundException;
+            import java.io.FileOutputStream;
+            import java.io.IOException;
+            import java.util.zip.ZipEntry;
+            import java.util.zip.ZipInputStream;
+
+            public class ArchiveUtil {
+
+                private ArchiveUtil() {}
+
+
+                public static void unzip(File directory, File zipFile) throws IOException {
+                    if(!directory.exists()) {
+                        directory.mkdirs();
+                    }
+                    byte[] buffer = new byte[2048];
+
+                    try (final FileInputStream fInput = new FileInputStream(zipFile);
+                         final ZipInputStream zipInput = new ZipInputStream(fInput)) {
+
+                        ZipEntry entry = zipInput.getNextEntry();
+                        while (entry != null) {
+                            String entryName = entry.getName();
+                            File file = new File(directory.getAbsolutePath() + File.separator + entryName);
+
+                            // Validate against potentially malicious zip payload
+                            // https://vulncat.fortify.com/en/detail?id=desc.controlflow.java.path_manipulation_zip_entry_overwrite
+                            if (!file.getCanonicalPath().startsWith(directory.getCanonicalPath())) {
+                                throw new IOException("The archive contains an entry that would be extracted outside of the target directory.");
+                            }
+
+                            if (entry.isDirectory()) {
+                                File newDir = new File(file.getAbsolutePath());
+                                if (!newDir.exists()) {
+                                    newDir.mkdirs();
+                                }
+                            } else {
+                                if (!file.getParentFile().isDirectory() && !file.getParentFile().exists()) {
+                                    file.getParentFile().mkdirs();
+                                }
+                                try (FileOutputStream fOutput = new FileOutputStream(file)) {
+                                    int count;
+                                    while ((count = zipInput.read(buffer)) > 0) {
+                                        fOutput.write(buffer, 0, count);
+                                    }
+                                }
+                            }
+                            zipInput.closeEntry();
+                            entry = zipInput.getNextEntry();
+                        }
+                        zipInput.closeEntry();
+                    } catch (IOException e) {
+                        throw new IOException(e);
+                    }
+                }
+
+            }
+            """,
+            """
+            import java.io.File;
+            import java.io.FileInputStream;
+            import java.io.FileNotFoundException;
+            import java.io.FileOutputStream;
+            import java.io.IOException;
+            import java.util.zip.ZipEntry;
+            import java.util.zip.ZipInputStream;
+
+            public class ArchiveUtil {
+
+                private ArchiveUtil() {}
+
+
+                public static void unzip(File directory, File zipFile) throws IOException {
+                    if(!directory.exists()) {
+                        directory.mkdirs();
+                    }
+                    byte[] buffer = new byte[2048];
+
+                    try (final FileInputStream fInput = new FileInputStream(zipFile);
+                         final ZipInputStream zipInput = new ZipInputStream(fInput)) {
+
+                        ZipEntry entry = zipInput.getNextEntry();
+                        while (entry != null) {
+                            String entryName = entry.getName();
+                            File file = new File(directory.getAbsolutePath() + File.separator + entryName);
+
+                            // Validate against potentially malicious zip payload
+                            // https://vulncat.fortify.com/en/detail?id=desc.controlflow.java.path_manipulation_zip_entry_overwrite
+                            if (!file.getCanonicalFile().toPath().startsWith(directory.getCanonicalFile().toPath())) {
+                                throw new IOException("The archive contains an entry that would be extracted outside of the target directory.");
+                            }
+
+                            if (entry.isDirectory()) {
+                                File newDir = new File(file.getAbsolutePath());
+                                if (!newDir.exists()) {
+                                    newDir.mkdirs();
+                                }
+                            } else {
+                                if (!file.getParentFile().isDirectory() && !file.getParentFile().exists()) {
+                                    file.getParentFile().mkdirs();
+                                }
+                                try (FileOutputStream fOutput = new FileOutputStream(file)) {
+                                    int count;
+                                    while ((count = zipInput.read(buffer)) > 0) {
+                                        fOutput.write(buffer, 0, count);
+                                    }
+                                }
+                            }
+                            zipInput.closeEntry();
+                            entry = zipInput.getNextEntry();
+                        }
+                        zipInput.closeEntry();
+                    } catch (IOException e) {
+                        throw new IOException(e);
+                    }
+                }
+
+            }
+            """
+        )
+    )
 }
