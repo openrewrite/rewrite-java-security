fb76c3c7b27b7c1f77c99733511322af50967ea3
Tim te Beek <timtebeek@gmail.com>
2023-02-07 09:45:52 +0000

    refactor: Common static analysis issues
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/java/security/internal/FileConstructorFixVisitorTest.java
+++ src/test/java/org/openrewrite/java/security/internal/FileConstructorFixVisitorTest.java
@@ -25,7 +25,7 @@ class FileConstructorFixVisitorTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.recipe(RewriteTest.toRecipe(() -> new FileConstructorFixVisitor<>()));
+        spec.recipe(RewriteTest.toRecipe(FileConstructorFixVisitor::new));
     }
 
     @SuppressWarnings("UnnecessaryLocalVariable")
